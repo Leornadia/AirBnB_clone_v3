2011255f7e3af86cea149a9bc5ad62d04f4f05f7
Leornadia <leornadiajose@gmail.com>
2024-05-28 07:34:22 -0700

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-from flask import Flask
+"""
+Flask app setup for AirBnB clone
+"""
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -9,11 +12,16 @@ app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """Closes the storage on teardown"""
+    """Close storage session"""
     storage.close()
 
+@app.errorhandler(404)
+def not_found(error):
+    """Return a JSON-formatted 404 status code response"""
+    return jsonify({"error": "Not found"}), 404
+
 if __name__ == "__main__":
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.getenv("HBNB_API_PORT", 5000))
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', '5000'))
     app.run(host=host, port=port, threaded=True)
 
--- api/v1/view/__init__.py
+++ api/v1/view/__init__.py
@@ -1,9 +1,2 @@
-#!/usr/bin/python3
-from flask import Blueprint
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-# Importing the routes
-from api.v1.views.index import *
 
 
